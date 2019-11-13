1efff98c4849583ccb56902cf8ccc036f574d581
Devynn Palmer <palmerdevynn@gmail.com>
2019-11-13 17:48:21 -0600

    tony fixxxx

--- univeristy-connect/src/DBConnect/UniversityConnectDatabaseConnect.js
+++ univeristy-connect/src/DBConnect/UniversityConnectDatabaseConnect.js
@@ -12,15 +12,72 @@ var con = mysql.createConnection({
     database: "universityconnect" // use the specified database
   });
 
-function promise
-
-function connect() {
+  function connect() {
     con.connect(function(err) {
     if (err) throw err;
-    // if connection is successful
-   console.log('connection successful');
   });
 }
+
+  promise.then(connect() {
+    console.log('connection sucessful');
+  });  
+
+query(sql, args) {
+  return new Promise((resolve, reject) => {
+      this.connection.query(sql, args, (err, rows) => {
+          if(err)
+              return reject(err);
+          resolve(rows);
+      });
+  });
+}
+
+close() {
+  return new Promise((resolve, reject) => {
+      this.connection.end( err => {
+          if(err)
+              return reject(err);
+          resolve();
+      });
+  });
+}
+
+  let someRows, otherRows
+
+  con.query(query)
+    .then(rows => {
+        someRows = rows;
+        return database.query(Query);
+    })
+    .then(rows => {
+        otherRows = rows;
+        return database.close();
+    }, err => {
+        return database.close().then(() => { 
+          throw err; 
+        })
+    })
+    .then(() => {
+        // do something with someRows and otherRows
+    }
+    .catch(err => {
+        // handle the error
+    })
+
+    con.execute( config,
+      database => database.query(query)
+      .then( rows => {
+          someRows = rows;
+          return database.query(query)
+      } )
+      .then( rows => {
+          otherRows = rows;
+      } )
+  ).then( () => {
+      // do something with someRows and otherRows
+  } ).catch( err => {
+      // handle the error
+  } );
 
   //function to retrieve data execute query
   //check for parameters first
